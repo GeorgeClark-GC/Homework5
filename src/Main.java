1ed01584ec24dcd670894314365d18eba3d427ce
GeorgeClark-GC <georgeclark2349@gmail.com>
2024-06-29 10:27:40 +0300

    Ex1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int clientOS = 1;
+        int clientDeviceYear = 2015;
+        if (clientOS == 0)
+        {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        }
+        else if (clientOS == 1)
+        {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
+        switch (clientOS)
+        {
+            case 0:
+                if (clientDeviceYear > 2014)
+                {
+                    System.out.println("Установите версию приложения для iOS по ссылке");
+                }
+                else
+                    System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+            case 1:
+                if (clientDeviceYear > 2014)
+                {
+                    System.out.println("Установите версию приложения для Android по ссылке");
+                }
+                else
+                    System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        }
     }
 }
